650a756a1cd49d43e2fb14dbdbb99545b28c1d08
cwong2015 <sirhc8002@gmail.com>
2017-12-28 17:16:28 -0500

    finished back-end logic! just need to slap on some front-end

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var client = new GoogleImages('001310055279050736320:1rkhztthlfo', 'AIzaSyC7YbQe
 
 var app = express();
 
-mongoose.connect('');
+mongoose.connect('mongodb://username:password@ds135817.mlab.com:35817/imagesearch');
 
 var schema = new mongoose.Schema({
     query: String
 });
 
-mongoose.model("ImageSearch", schema);
+var dbUrl = mongoose.model("ImageSearch", schema);
 
 app.get('/api/GoogleImages/:query', function(req, res){
     var page = req.query.offset;
@@ -29,12 +29,45 @@ app.get('/api/GoogleImages/:query', function(req, res){
             };
             relevantData.push(jsonObj);
         }
+        dbUrl({query: topic}).save(function(err, data){
+            console.log(data);
+            if(err){
+                throw err;
+            }
+            else{
+                // console.log(data + " has been saved.");
+            }
+        });
         res.json(relevantData);
     });
 });
 
 app.get('/api/recent/', function(req, res){
-
+    // CREDITS TO THIS STACK OVERFLOW POST: https://stackoverflow.com/questions/19584814/select-last-10-entries-in-mongoose
+    var query = dbUrl.find({}, null, {limit: 10, sort: {'_id': -1}});
+    query.exec(function(err, data){
+    // end credit
+        var dataArr = [];
+        if(err){
+            throw err;
+        }
+        else{
+            var numDisplay;
+            if(data.length >= 10){
+                numDisplay = 9;
+            }
+            else{
+                numDisplay = data.length - 1;
+            }
+            for(var i = numDisplay; i >= 0; i--){
+                var jsonObj = {
+                    query: data[i]
+                };
+                dataArr.push(jsonObj);
+            }
+        }
+        res.json(dataArr.reverse());
+    });
 });
 
 app.listen(8003);
