cf6ec80fe991dcc5a34617defeee860eb9d284ff
cwong2015 <sirhc8002@gmail.com>
2017-12-28 15:15:02 -0500

    sweet, finally got API to work -- needed to get API key, CX code, and used google-image API instead of google-search

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var express = require('express');
-var GoogleSearch = require('google-search');
+var GoogleImages = require('google-images');
+var client = new GoogleImages('001310055279050736320:1rkhztthlfo', 'AIzaSyC7YbQe674pVlTDHaZJN9j4-kASOVNCLiQ');
 
-var googleSearch = new GoogleSearch({
-    key: 'ABSTRACTED',
-    cx: 'ABSTRACTED'
+var app = express();
+
+app.get('/api/GoogleImages/:query', function(req, res){
+    var page = req.query.offset;
+    if(page == null){
+        page = 1;
+    }
+    var topic = req.params.query;
+
+    client.search(topic, {page: page}).then(function(image){
+        res.json(image);
+    });
 });
 
+app.listen(8003);
